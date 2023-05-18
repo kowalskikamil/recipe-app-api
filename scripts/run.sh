f2248af0c93a3e8630160732e5be829bb5138eae
PROlikeG6 <prolikeg5@gmail.com>
2023-05-18 19:45:48 +0900

    Further implementation

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+#echo "Displaying username:"
+#whoami
+#mkdir /vol/web/static/admin
+#echo "displaying permissions to /vol/web"
+#ls -la /vol/web
+#echo "displaying permissions to /vol/web/static/admin"
+#ls -la /vol/web/static/admin
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
